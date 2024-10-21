568c591161bf2798ea769d5c53d34982fb5a92e7
gns168 <gns1688@gmail.com>
2024-10-21 13:21:36 -0400

    t2

---
+++ script/our_story.js
@@ -0,0 +1,14 @@
+
+/*  1.0 Header resize */
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
+
+/* 2.0 Slide */
